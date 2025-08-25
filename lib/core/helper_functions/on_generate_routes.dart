24d081eadb79346013c2e61ba9ef7690083ad43e
kareemAlfara <kareemelfara@gmail.com>
2025-08-25 04:46:53 +0300

    Add route handling for authentication and onboarding views; update shared preferences initialization

--- lib/core/helper_functions/on_generate_routes.dart
+++ lib/core/helper_functions/on_generate_routes.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:fruits_hub/features/auth/presentation/view/forgetPassword.dart';
+import 'package:fruits_hub/features/auth/presentation/view/signinView.dart';
+import 'package:fruits_hub/features/auth/presentation/view/signupView.dart';
 import 'package:fruits_hub/features/onboard/presentation/view/onboarding.dart';
 import 'package:fruits_hub/features/splash/presentation/view/splash_view.dart';
+
+import '../../features/auth/presentation/view/verification.dart';
 
 class OnGenerateRoutes {
   static Route<dynamic> generateRoute(RouteSettings settings) {
@@ -9,6 +14,14 @@ class OnGenerateRoutes {
         return MaterialPageRoute(builder: (_) => const SplashView());
       case Onboarding.routeName:
         return MaterialPageRoute(builder: (_) => const Onboarding());
+      case Signinview.routeName:
+        return MaterialPageRoute(builder: (_) => const Signinview());
+      case Signupview.routeName:
+        return MaterialPageRoute(builder: (_) => const Signupview());
+      case ForgetPasswordView.routeName:
+        return MaterialPageRoute(builder: (_) => const ForgetPasswordView());
+      case VerificationView.routeName:
+        return MaterialPageRoute(builder: (_) => const VerificationView());
       default:
         return MaterialPageRoute(builder: (_) => const Scaffold());
     }
--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:fruits_hub/core/helper_functions/on_generate_routes.dart';
+import 'package:fruits_hub/core/services/Shared_preferences.dart';
 import 'package:fruits_hub/core/utils/app_colors.dart';
-import 'package:fruits_hub/features/onboard/presentation/view/widget/page_view_item.dart';
-import 'package:fruits_hub/features/splash/presentation/view/splash_view.dart';
 import 'package:fruits_hub/generated/l10n.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
 import 'features/onboard/presentation/view/onboarding.dart';
 
-void main(List<String> args) {
+void main(List<String> args)async {
+  await SharedPreferencesSingleton.init();
   runApp(const MyApp());
 }
 class MyApp extends StatelessWidget {
